fbc52260b89256112d78dedac5f1390eff4e91da
raul.zamora <raul.zamora@cbtis269.edu.mx>
2026-01-30 20:31:03 +0000

    ejemplo AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MiTienda());
+
+class MiTienda extends StatelessWidget {
+  const MiTienda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Raul CBTis 269'), centerTitle: true),
-        body: Center(child: Text("Bienvenido Raul Cbtis 269")),
+        appBar: AppBar(
+          leading: Icon(Icons.account_circle_rounded),
+          leadingWidth: 100, // default is 56
+          title: Text('Tienda de Raul 269'),
+          backgroundColor: const Color.fromARGB(255, 207, 255, 34),
+            actions: [
+                Icon(Icons.more_vert),
+            ],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Lista de productos disponibles"),
+        ),
       ),
-    ),
-  );
-} //Fin de MAIN
+    );
+  }
+}
